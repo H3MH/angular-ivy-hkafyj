7781dcc87d124165d0d00eb8802f932de67ac78a
H3MH <hbmh.sc@hotmail.com>
2020-09-08 05:28:07 -0400

    3 binaries uploaded

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,24 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
+
+// third-party
+import { AnimateOnScrollModule } from 'ng2-animate-on-scroll';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    AnimateOnScrollModule.forRoot()
+  ],
+  providers: [ ],
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
